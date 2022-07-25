bf3d891ed93f1664cb7ce0dc7dd52fb219a064f0
Ifeanyi Osi-Okeke <ifeanyi.osi.okeke@gmail.com>
2022-07-25 13:20:35 +0100

    ....

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-
+import React from 'react'
+// import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className='p-6 max-w-sm mx-auto bg-red-700 rounded-xl
+    shadow-md flex items-center space-x-4
+    '>
+      <div>
+        <div className='text-xl font-medium text-black'>chitchat</div>
+        <div>
+          <p className='text-grey-500'>new message</p>
+        </div>
+      </div>
+
+      
       
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
