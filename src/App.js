2ddd88b0c04eb32d91f762a20d70588ca2bb0e9f
Ifeanyi Osi-Okeke <ifeanyi.osi.okeke@gmail.com>
2022-07-25 12:07:03 +0100

    ....

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Content from './Components/Content';
-import Navbar from './Components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Content />
+      
     </div>
   );
 }
---
+++ src/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Karla: ["Karla", "sans-serif"],
+        Beautiste: ["Beautiste", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
